1ab4376b585400356f570d0d33cad1eaf31c302c
mcauzzi <m.cauzzi93@gmail.com>
2025-04-28 16:21:16 +0200

    Added MyBox

---
+++ BoxT/src/main.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+use std::ops::Deref;
+
+use crate::List::{Cons, Nil};
+fn main() {
+    let b = Box::new(5);
+    println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("Cons value= {:?}", list);
+    let x = 5;
+    let y = Box::new(x);
+    let my_y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *my_y);
+
+    let name=MyBox::new(String::from("Rust"));
+    hello(&name);
+}
+
+fn hello(name:&str){
+    println!("Hello {name}")
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
